abc9cd9bced4748c5bf7a423d1674b2e3b6db80f
Tamme Schichler <tamme@schichler.dev>
2021-01-26 16:52:30 +0100

    This basically works for my purposes now, I think

--- src/lib.rs
+++ src/lib.rs
@@ -2,17 +2,20 @@
 #![warn(clippy::pedantic)]
 #![allow(clippy::missing_errors_doc)]
 
+use chrono::NaiveDateTime;
+use enumflags2::BitFlags;
 use std::{
 	any::type_name,
-	borrow::Borrow,
+	borrow::{Borrow, Cow},
 	convert::{TryFrom, TryInto},
 	ffi::{OsStr, OsString},
 	fmt::{Debug, Display, Formatter},
 	io::{Error, ErrorKind},
 	iter,
-	ops::{Deref, Index, Range, RangeFrom, RangeTo},
+	ops::{Add, Deref, Index, Range, RangeFrom, RangeInclusive, RangeTo},
+	path::PathBuf,
 	process::{Command, Output},
-	time::{Duration, UNIX_EPOCH},
+	time::{Duration, SystemTime, UNIX_EPOCH},
 };
 
 #[cfg(doctest)]
@@ -22,6 +25,7 @@ pub mod readme {
 
 macro_rules! unix_mode_fn {
 	($name:ident) => {
+		#[must_use]
 		pub fn $name(&self) -> bool {
 			unix_mode::$name(self.0)
 		}
@@ -29,6 +33,17 @@ macro_rules! unix_mode_fn {
 	($($name:ident,)*) => {
 		$(unix_mode_fn!($name);)*
 	};
+}
+
+#[derive(BitFlags, Clone, Copy, Debug, PartialEq)]
+pub enum ModeKind {
+	BlockDevice = 1 << 0,
+	CharDevice = 1 << 1,
+	Dir = 1 << 2,
+	Fifo = 1 << 3,
+	File = 1 << 4,
+	Socket = 1 << 5,
+	Symlink = 1 << 6,
 }
 
 pub struct UnixMode(u32);
@@ -46,12 +61,43 @@ impl UnixMode {
 		is_socket,
 		is_symlink,
 	}
-	pub fn to_string(&self) -> String {
-		unix_mode::to_string(self.0)
-	}
 
+	#[must_use]
 	pub fn to_u32(&self) -> u32 {
 		self.0
+	}
+
+	#[must_use]
+	pub fn kind(&self) -> BitFlags<ModeKind> {
+		let mut result = BitFlags::empty();
+		if self.is_block_device() {
+			result |= ModeKind::BlockDevice
+		}
+		if self.is_char_device() {
+			result |= ModeKind::CharDevice
+		}
+		if self.is_dir() {
+			result |= ModeKind::Dir
+		}
+		if self.is_fifo() {
+			result |= ModeKind::Fifo
+		}
+		if self.is_file() {
+			result |= ModeKind::File
+		}
+		if self.is_socket() {
+			result |= ModeKind::Socket
+		}
+		if self.is_symlink() {
+			result |= ModeKind::Symlink
+		}
+		result
+	}
+}
+impl ToString for UnixMode {
+	#[must_use]
+	fn to_string(&self) -> String {
+		unix_mode::to_string(self.0)
 	}
 }
 
@@ -63,10 +109,17 @@ impl Debug for UnixMode {
 
 #[derive(Debug)]
 pub struct LsEntry {
-	mode: UnixMode,
-	size: u32,
-	epoch: Epoch,
-	name: RawString,
+	pub mode: UnixMode,
+	pub size: u32,
+	pub epoch: Epoch,
+	pub name: RawString,
+}
+
+impl UnixMode {
+	#[must_use]
+	pub fn permissions(&self) -> u32 {
+		self.0 & 0o_777
+	}
 }
 
 #[derive(Debug)]
@@ -75,6 +128,121 @@ impl Epoch {
 	#[must_use]
 	pub fn from_timestamp(secs: u32) -> Self {
 		Self(secs)
+	}
+}
+
+impl RawPath {
+	#[must_use]
+	pub fn directory(&self) -> Option<&Self> {
+		self.iter().rposition(|b| *b == b'/').map(|p| &self[0..=p])
+	}
+
+	#[must_use]
+	pub fn without_prefix(&self, prefix: &Self) -> &Self {
+		assert!(self.starts_with(&prefix.0 .0));
+		&self[prefix.len()..]
+	}
+}
+
+impl<I> Index<I> for RawPath
+where
+	RawStr: Index<I, Output = RawStr>,
+{
+	type Output = RawPath;
+
+	fn index(&self, index: I) -> &Self::Output {
+		(&self.0[index]).into()
+	}
+}
+
+impl Epoch {
+	#[must_use]
+	pub fn to_date_time(&self) -> NaiveDateTime {
+		NaiveDateTime::from_timestamp(i64::from(self.0), 0)
+	}
+}
+
+impl RawPath {
+	#[must_use]
+	pub fn to_string_panicky(&self) -> String {
+		self.0.to_string_panicky()
+	}
+}
+
+impl RawStr {
+	#[must_use]
+	pub fn to_string_panicky(&self) -> String {
+		std::string::String::from_utf8(self.to_vec()).unwrap()
+	}
+}
+
+impl PartialEq<&str> for RawStr {
+	fn eq(&self, other: &&str) -> bool {
+		self.0 == *other.as_bytes()
+	}
+}
+
+impl<T: ?Sized> PartialEq<T> for RawString
+where
+	RawStr: PartialEq<T>,
+{
+	fn eq(&self, other: &T) -> bool {
+		self.as_str() == other
+	}
+}
+
+impl RawString {
+	pub fn as_str(&self) -> &RawStr {
+		RawStr::new(&self.0)
+	}
+}
+
+impl<'a> From<&'a RawStr> for &'a RawPath {
+	fn from(str: &'a RawStr) -> Self {
+		RawPath::new(&str.0)
+	}
+}
+
+impl RawPath {
+	pub fn join<'a>(&self, other: impl Into<&'a RawPath>) -> RawPathBuf {
+		self.join_impl(other.into())
+	}
+
+	pub fn join_impl(&self, other: &RawPath) -> RawPathBuf {
+		let mut slash = 1;
+		if self.ends_with(&[b'/']) {
+			slash -= 1
+		}
+		if other.ends_with(&[b'/']) {
+			slash -= 1
+		}
+		let mut result = Vec::new();
+		result.extend(&self.0 .0);
+		while slash > 0 {
+			result.push(b'/');
+			slash -= 1;
+		}
+		while slash < 0 {
+			assert_eq!(result.pop(), Some(b'/'));
+			slash += 1;
+		}
+		assert_eq!(slash, 0);
+		result.extend(&other.0 .0);
+		RawPathBuf(RawString(result))
+	}
+}
+
+impl Borrow<RawPath> for RawPathBuf {
+	fn borrow(&self) -> &RawPath {
+		RawPath::new(&self.0 .0)
+	}
+}
+
+impl ToOwned for RawPath {
+	type Owned = RawPathBuf;
+
+	fn to_owned(&self) -> Self::Owned {
+		RawPathBuf(self.0.to_owned())
 	}
 }
 
@@ -233,13 +401,11 @@ fn scrape<'a>(
 	command: &(impl AsRef<OsStr> + ?Sized),
 	args: impl IntoIterator<Item = &'a RawStr>,
 ) -> Result<Vec<u8>, Error> {
-	let output = Command::new("adb")
-		.args(
-			args.into_iter()
-				.map(|s| OsString::try_from(s))
-				.collect::<Result<Vec<_>, _>>()?,
-		)
-		.output()?;
+	let args = args
+		.into_iter()
+		.map(OsString::try_from)
+		.collect::<Result<Vec<OsString>, _>>()?;
+	let output = Command::new("adb").args(args.iter()).output()?;
 	if output.status.success() {
 		Ok(output.stdout)
 	} else {
@@ -266,14 +432,53 @@ impl Display for ExitError {
 }
 
 #[repr(transparent)]
-pub struct RawPathBuf(Vec<u8>);
+pub struct RawPathBuf(RawString);
 
+impl Debug for RawPathBuf {
+	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+		<RawPath as Debug>::fmt(&self, f)
+	}
+}
+
+impl Deref for RawPathBuf {
+	type Target = RawPath;
+
+	fn deref(&self) -> &Self::Target {
+		RawPath::new(&self.0 .0)
+	}
+}
+
+impl From<&str> for RawPathBuf {
+	fn from(str: &str) -> Self {
+		Self(str.into())
+	}
+}
+
+impl From<&str> for RawString {
+	fn from(str: &str) -> Self {
+		Self(str.into())
+	}
+}
+
+#[derive(Debug)]
 #[repr(transparent)]
 pub struct RawPath(RawStr);
 impl RawPath {
 	pub fn new(data: &(impl AsRef<[u8]> + ?Sized)) -> &Self {
 		let ptr = RawStr::new(data.as_ref()) as *const _ as *const RawPath;
 		unsafe { &*ptr }
+	}
+}
+
+impl AsRef<RawPath> for RawPath {
+	fn as_ref(&self) -> &RawPath {
+		self
+	}
+}
+
+impl<'a> From<&'a str> for &'a RawPath {
+	fn from(str: &'a str) -> Self {
+		RawPath::new(str.as_bytes())
 	}
 }
 
@@ -389,8 +594,54 @@ impl Index<RangeFrom<usize>> for RawStr {
 	}
 }
 
+impl Index<RangeInclusive<usize>> for RawStr {
+	type Output = RawStr;
+
+	fn index(&self, index: RangeInclusive<usize>) -> &Self::Output {
+		RawStr::new(&self.0[index])
+	}
+}
+
 impl Debug for RawString {
 	fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
 		Debug::fmt(&String::from_utf8_lossy(&self.0), f)
 	}
+}
+
+pub fn pull(
+	serial_number: &SerialNumber,
+	path: &(impl AsRef<RawPath> + ?Sized),
+) -> Result<Vec<u8>, Error> {
+	pull_impl(serial_number, path.as_ref())
+}
+
+pub fn pull_impl(serial_number: &SerialNumber, path: &RawPath) -> Result<Vec<u8>, Error> {
+	let _it_works = scrape(
+		"adb",
+		[
+			RawStr::new("-s"),
+			serial_number,
+			RawStr::new("shell"),
+			RawStr::new("-n"),
+			RawStr::new("-T"),
+			RawStr::new("cat"),
+			RawStr::new(shell_escape::escape(Cow::Owned(path.to_string_panicky())).as_bytes()),
+		]
+		.iter()
+		.copied(),
+	)?;
+
+	scrape(
+		"adb",
+		[
+			RawStr::new("-s"),
+			serial_number,
+			RawStr::new("exec-out"),
+			RawStr::new("cat"),
+			// Must not be escaped!
+			path,
+		]
+		.iter()
+		.copied(),
+	)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,112 @@
 #![warn(clippy::pedantic)]
 
-use std::io::Error;
+use adb_dump::{LsEntry, ModeKind, RawPath, SerialNumber};
+use chrono::{Datelike, NaiveDateTime, Timelike};
+use std::convert::TryFrom;
+use std::{
+	fs::File,
+	io::{Error, Seek, Write},
+};
+use zip::{write::FileOptions, CompressionMethod, DateTime, ZipWriter};
 
 fn main() -> Result<(), Error> {
 	let s_no = dbg!(adb_dump::get_serialno())?;
-	dbg!(adb_dump::ls(&s_no, "/")?.collect::<Vec<_>>());
+
+	let arg_path: &RawPath = "/data".into();
+	let prefix = arg_path.directory().unwrap();
+
+	let mut zip = ZipWriter::new(File::create("backup.zip")?);
+
+	visit_dir(&mut zip, &s_no, prefix, arg_path)?;
+
+	zip.finish()?;
+
+	Ok(())
+}
+
+fn convert_date_time(date_time: &NaiveDateTime) -> DateTime {
+	dbg!(date_time);
+	let time = date_time.time();
+	DateTime::from_date_and_time(
+		u16::try_from(date_time.year()).unwrap(),
+		u8::try_from(date_time.month()).unwrap(),
+		u8::try_from(date_time.day()).unwrap(),
+		u8::try_from(time.hour()).unwrap(),
+		u8::try_from(time.minute()).unwrap(),
+		u8::try_from(time.second()).unwrap(),
+	)
+	.unwrap_or_else(|()| {
+		let default = DateTime::default();
+		eprintln! {"Ignoring date: {:?}, using {:?} instead", date_time,default};
+		default
+	})
+}
+
+fn visit_dir<W: Write + Seek>(
+	zip: &mut ZipWriter<W>,
+	serial_number: &SerialNumber,
+	archive_root: &RawPath,
+	path: &RawPath,
+) -> Result<(), Error> {
+	println!("dir {:?}", &path);
+
+	for entry in adb_dump::ls(serial_number, path)? {
+		match entry.mode.kind() {
+			dir if dir == ModeKind::Dir => {
+				if entry.name != "." && entry.name != ".." {
+					let timestamp = convert_date_time(&entry.epoch.to_date_time());
+					zip.add_directory(
+						path.without_prefix(archive_root)
+							.join(entry.name.as_str())
+							.to_string_panicky(),
+						FileOptions::default()
+							.compression_method(CompressionMethod::Stored)
+							.last_modified_time(timestamp)
+							.unix_permissions(entry.mode.permissions()),
+					)?;
+					//TODO: Add dir!
+					visit_dir(
+						zip,
+						serial_number,
+						archive_root,
+						&path.join(entry.name.as_str()),
+					)?
+				}
+			}
+			file if file == ModeKind::File => visit_file(
+				zip,
+				serial_number,
+				archive_root,
+				&path.join(entry.name.as_str()),
+				&entry,
+			)?,
+			// other => todo!("{:?}", other),
+			other => {
+				eprintln!("{:?}", (other, entry.name));
+			}
+		}
+	}
+
+	Ok(())
+}
+
+fn visit_file<W: Write + Seek>(
+	zip: &mut ZipWriter<W>,
+	serial_number: &SerialNumber,
+	archive_root: &RawPath,
+	path: &RawPath,
+	entry: &LsEntry,
+) -> Result<(), Error> {
+	println!("file {:?}", &path);
+
+	zip.start_file(
+		path.without_prefix(archive_root).to_string_panicky(),
+		FileOptions::default()
+			.compression_method(CompressionMethod::Stored)
+			.last_modified_time(convert_date_time(&entry.epoch.to_date_time()))
+			.unix_permissions(entry.mode.permissions()),
+	)?;
+	zip.write_all(&adb_dump::pull(serial_number, path)?)?;
+	zip.flush()?;
 	Ok(())
 }
